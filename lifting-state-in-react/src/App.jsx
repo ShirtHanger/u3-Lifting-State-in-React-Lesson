bb79c9e5efd2c0f5b4335656add6d1b45da0f8aa
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-10-31 12:52:46 -0500

    Part 1 done

--- lifting-state-in-react/src/App.jsx
+++ lifting-state-in-react/src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import IncrementButton from './components/IncrementButton';
 
 const App = () => {
 
+  const [count, setCount] = useState(0)
+
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Hello world!</h1>
+      <p>Count: {count}</p>
+      <IncrementButton addOne={addOne} /> {/* Passing in a function as a PROP for child component */}
+    </>
   );
+
+  function addOne() {
+    setCount(count + 1)
+  }
 };
 
 export default App;
---
+++ lifting-state-in-react/src/components/IncrementButton.jsx
@@ -0,0 +1,10 @@
+// src/components/IncrementButton.jsx
+
+const IncrementButton = (props) => { /* Given multiple functions/objects, it may be easier to use the props dot notation */
+    return (
+      <button onClick={props.addOne}>Increment</button>
+    );
+  };
+  
+  export default IncrementButton;
+  
