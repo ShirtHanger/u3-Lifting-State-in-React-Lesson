dd25364f9be3ef17c9d1aabbd26b751cd178e0ee
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-10-31 13:20:37 -0500

    Part 3 done

--- lifting-state-in-react/src/App.jsx
+++ lifting-state-in-react/src/App.jsx
@@ -4,15 +4,31 @@ import NewTodo from './components/NewTodo'
 
 const App = () => {
 
+  const [todos, setTodos] = useState([]);
+  /* Array of Todo Strings */
+
   return (
     <>
       <h1>To-do App</h1>
       <NewTodo addTodo={addTodo} /> {/* Passes addTodo function DOWNWARDS for use in the NewTodo.jsx */}
+      <ul>
+        {todos.map((todo, index) => {
+          return <li key={index}>{todo}</li>;
+        })}
+      </ul>
     </>
   )
 
   function addTodo(newTodo) {
-    console.log(newTodo)
+    /* THIS is bad practice, do NOT update a state directly. Just replace */
+    /* todos.push(newTodo); */
+    const updatedTodos = [...todos, newTodo]; /* New array, adds new object to the new array */
+    console.log(todos);
+    console.log(updatedTodos);
+    /* Replace old array with new array */
+    setTodos(updatedTodos);
+    /* This works too */
+    /* setTodos([...todos, newTodo]) */
   }
 }
 
